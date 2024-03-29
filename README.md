3ec4a54a709b04385d25c9897ee491729343391a
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-03-29 14:40:14 +0100

    Add url property

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-GS1 Schema Downloader Maven Plugin 
+# GS1 Schema Downloader Maven Plugin 
 
+## Run the plugin locally
+```shell
+mvn com.eightbits.commerce.retail:gs1-schema-downloader:download-gs1-schema
+```
+### Configuration
+```shell
+mvn com.eightbits.commerce.retail:gs1-schema-downloader:download-gs1-schema -Dgs1SchemaUrl=https://www.gs1.org/voc/ -DoutputDirectory=target/generated-resources/gs1-schema
+```
+
+## Run the plugin in a project
+```xml
+<plugin>
+    <groupId>com.eightbits.commerce.retail</groupId>
+    <artifactId>gs1-schema-downloader</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <executions>
+        <execution>
+            <id>download-gs1-schema</id>
+            <phase>generate-resources</phase>
+            <goals>
+                <goal>download-gs1-schema</goal>
+            </goals>
+        </execution>
+    </executions>
+</plugin>
+```
+
+## Configuration
+```xml
+<configuration>
+    <gs1SchemaUrl>https://www.gs1.org/voc/</gs1SchemaUrl>
+    <outputDirectory>${project.build.directory}/generated-resources/gs1-schema</outputDirectory>
+</configuration>
+```
+
+## Output
+The plugin will download the GS1 schema from the provided URL and save it in the output directory. The schema will be saved in the following format:
+```
+outputDirectory
+├── gs1-schema.json
+``` 
--- src/main/java/com/eightbits/commerce/retail/GS1SchemaDownloader.java
+++ src/main/java/com/eightbits/commerce/retail/GS1SchemaDownloader.java
@@ -6,17 +6,65 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 @Mojo(name = "download-gs1-schema", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
 public class GS1SchemaDownloader extends AbstractMojo {
 
-    @Parameter(property = "download.url", defaultValue = "http://www.gdsregistry.org/3.1/schemas/")
+    @Parameter(property = "gs1SchemaUrl", defaultValue = "http://www.gdsregistry.org/3.1/schemas/")
     private String url;
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+//    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+//    MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("Downloading GS1 schema...");
+        getLog().info("URL: " + url);
+        countNumberOfDependencies();
+        try {
+            downloadGS1Schema();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error downloading GS1 schema", e);
+        }
+
+
+//        try {
+//            Process process = new ProcessBuilder("python", "download_gs1_schema.py").start();
+//            process.waitFor();
+//        } catch (IOException | InterruptedException e) {
+//            throw new MojoExecutionException("Error running Python script", e);
+//        }
+    }
+
+    private void downloadGS1Schema() throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
+        links.forEach(link -> {
+            String href = link.attr("href");
+            getLog().info("Link: " + href);
+            if (href.endsWith(".xsd")) {
+                getLog().info("Downloading: " + href);
+//                try {
+//                    Jsoup.connect(url + href).ignoreContentType(true).execute().bodyAsBytes();
+//                } catch (IOException e) {
+//                    getLog().error("Error downloading: " + href, e);
+//                }
+            }
+        });
+    }
+
+    private void countNumberOfDependencies() {
+        getLog().info("Project: " + project.getName());
+        int count = project.getDependencies().size();
+        getLog().info("Number of project dependencies: " + count);
     }
 }
